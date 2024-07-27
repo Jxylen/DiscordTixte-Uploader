e5ae790696ff0b5d762c80d319037cc7014a8b92
jaylen <jaylenjruiz@gmail.com>
2024-07-27 02:20:29 -0400

    added a link button to uploaded content

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,35 +1,46 @@
-const { Events, Message } = require("discord.js");
-const uploadDomain = "cdn.jaylen.nyc" // Change this to the domain you want to upload this with
+const { Events, Message, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
+const uploadDomain = "cdn.jaylen.nyc"; // Change this to the domain you want to upload this with
 
 module.exports = {
   name: Events.MessageCreate,
   /**
-   * 
+   *
    * @param {Message} message - The message instance
    */
   async execute(message, client) {
-    const fetch = (await import('node-fetch')).default;
+    const fetch = (await import("node-fetch")).default;
 
     // Check if the message has attachments or is a specific command
     if (message.attachments.size === 0) return;
-    if (!message.mentions.has(client.user) && message.content !== "<@1262217161317814394>") return;
+    if (
+      !message.mentions.has(client.user) &&
+      message.content !== "<@1262217161317814394>"
+    )
+      return;
 
     const attachment = message.attachments.first();
-    if (!attachment || !attachment.name.match(/\.(jpg|jpeg|png|gif|mp4)$/i)) return;
+    if (!attachment || !attachment.name.match(/\.(jpg|jpeg|png|gif|mp4)$/i))
+      return;
 
     // Extract the second word from the message content and will set it as a title IF there is one.
     const words = message.content.split(/\s+/);
-    const newFilename = words.length >= 2 ? words[1] + attachment.name.slice(attachment.name.lastIndexOf('.')) : attachment.name;
+    const newFilename =
+      words.length >= 2
+        ? words[1] + attachment.name.slice(attachment.name.lastIndexOf("."))
+        : attachment.name;
 
     const form = new FormData();
     const payloadJson = JSON.stringify({
       domain: uploadDomain,
-      name: newFilename
+      name: newFilename,
     });
 
     try {
       const res = await fetch(attachment.url);
-      if (!res.ok) throw new Error(`Failed to fetch ${newFilename}: ${res.status} - ${res.statusText}`);
+      if (!res.ok)
+        throw new Error(
+          `Failed to fetch ${newFilename}: ${res.status} - ${res.statusText}`
+        );
 
       const buffer = await res.arrayBuffer();
       const blob = new Blob([buffer], { type: attachment.contentType });
@@ -43,24 +54,37 @@ module.exports = {
       const uploadRes = await fetch(apiUrl, {
         method: "POST",
         headers: { Authorization: `${process.env.TIXTE_UPLOAD_KEY}` },
-        body: form
+        body: form,
       });
 
       if (!uploadRes.ok) {
-        console.error(`Failed to upload ${newFilename}: ${uploadRes.status} - ${uploadRes.statusText}`);
-        await message.react('❌');
-        await message.reply(`Error uploading \`${newFilename}\`: \`${uploadRes.status}\` - ${uploadRes.statusText}`);
+        console.error(
+          `Failed to upload ${newFilename}: ${uploadRes.status} - ${uploadRes.statusText}`
+        );
+        await message.react("❌");
+        await message.reply(
+          `Error uploading \`${newFilename}\`: \`${uploadRes.status}\` - ${uploadRes.statusText}`
+        );
         return;
       }
 
       const jsonResponse = await uploadRes.json();
-      
-      // React with checkmark and reply with success message
-      await message.react('✅');
-      await message.reply(`[${newFilename}](${jsonResponse.data.url}) uploaded successfully.`);
+      const LinkToVideo = new ButtonBuilder()
+        .setLabel(`Link to ${jsonResponse.data.name}`)
+        .setURL(`${jsonResponse.data.direct_url}`)
+        .setStyle(ButtonStyle.Link);
+
+        const row = new ActionRowBuilder()
+        .addComponents(LinkToVideo);
+
+      await message.react("✅");
+      await message.reply({
+        content: `**${jsonResponse.data.name}** uploaded successfully.`,
+        components: [row]
+      });
     } catch (error) {
       console.error(`Error uploading ${newFilename}:`, error);
-      await message.react('❌');
+      await message.react("❌");
     }
   },
 };
